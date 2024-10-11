f35ecda6468ab87005771f26be41156469290f81
Nikita Malinin <nikita.malinin@intel.com>
2024-10-11 11:22:34 +0200

    Another approach

--- nncf/quantization/algorithms/smooth_quant_tuner/algorithm.py
+++ nncf/quantization/algorithms/smooth_quant_tuner/algorithm.py
@@ -28,7 +28,7 @@ from nncf.common.tensor_statistics.statistic_point import StatisticPoint
 from nncf.common.tensor_statistics.statistic_point import StatisticPointsContainer
 from nncf.common.utils.backend import BackendType
 from nncf.openvino.graph.transformations.commands import OVTargetPoint
-from nncf.openvino.statistics.collectors import get_mean_statistic_collector
+from nncf.openvino.statistics.collectors import get_raw_stat_collector
 from nncf.quantization.advanced_parameters import AdvancedQuantizationParameters
 from nncf.quantization.algorithms.algorithm import Algorithm
 from nncf.quantization.algorithms.bias_correction.algorithm import BIAS_CORRECTION_THRESHOLD
@@ -44,7 +44,30 @@ from nncf.tensor import functions as fns
 TModel = TypeVar("TModel")
 TPass = Callable[[TModel], TModel]
 
-SCALES_GRID = list(np.arange(0, 1.01, 0.1))
+ALPHAS_GRID = [
+    0.0,
+    0.05,
+    0.1,
+    0.15,
+    0.2,
+    0.25,
+    0.3,
+    0.35,
+    0.4,
+    0.45,
+    0.5,
+    0.55,
+    0.6,
+    0.65,
+    0.7,
+    0.75,
+    0.8,
+    0.85,
+    0.9,
+    0.95,
+    1.0,
+]
+# ALPHAS_GRID = [0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]
 
 
 class SmoothQuantTuner(Algorithm):
@@ -60,7 +83,7 @@ class SmoothQuantTuner(Algorithm):
         advanced_parameters: Optional[AdvancedQuantizationParameters] = None,
     ):
         self._algorithm_key = f"SQ_{hash(self)}_TUNE"
-        self._scales_grid = SCALES_GRID
+        self._alphas_grid = ALPHAS_GRID
         self._subset_size = subset_size
 
         self._smooth_quant = None
@@ -131,27 +154,28 @@ class SmoothQuantTuner(Algorithm):
         sq_statistic_points = self._smooth_quant.get_statistic_points(model, graph)
         sq_statistics = self.collect_statistics(sq_statistic_points, model, graph, dataset)
 
-        scale_by_node_name = {}
         output_port_id = 0
 
-        nodes_to_tune = list(sq_statistic_points.data.keys())
+        alpha_map = self._smooth_quant._get_alpha_map()
+        nodes_to_smooth = self._smooth_quant._get_nodes_to_smooth_data(graph, alpha_map)
+        node_groups = self._smooth_quant._group_nodes_by_source(nodes_to_smooth, graph)
+        group_losses = {gid: np.inf for gid in node_groups}
 
-        for node_name in nodes_to_tune:
-            scale_by_node_name[node_name] = {"diff": np.inf, "scale": 0}
-        fp_container = self.get_container_for_nodes(
-            nodes_to_tune, self._algorithm_key, output_port_id, self._subset_size
-        )
+        nodes_list = list(sq_statistic_points.keys())
+        nodes_alpha = {n: 0 for n in nodes_list}
+
+        fp_container = self.get_container_for_nodes(nodes_list, self._algorithm_key, output_port_id, self._subset_size)
 
         nncf_logger.info(">>> Collecting FP statistics for tune")
         fp_post_layer_statistics = self.collect_statistics(fp_container, model, graph, dataset)
 
         nncf_logger.info(">>> Applying tuning from space:")
-        nncf_logger.info(self._scales_grid)
+        nncf_logger.info(self._alphas_grid)
 
-        for scale in self._scales_grid:
-            nncf_logger.info(f"Tuning with {scale} scale")
+        for alpha in self._alphas_grid:
+            nncf_logger.info(f"Tuning with {alpha} alpha")
             modified_model = deepcopy(model)
-            self._smooth_quant._alpha_map["matmul"] = scale
+            self._smooth_quant._alpha_map["matmul"] = alpha
 
             modified_nncf_graph = NNCFGraphFactory.create(modified_model)
             modified_model = self._smooth_quant.apply(modified_model, modified_nncf_graph, sq_statistics)
@@ -159,7 +183,7 @@ class SmoothQuantTuner(Algorithm):
             modified_nncf_graph = NNCFGraphFactory.create(modified_model)
             min_max_statistic_points = self._min_max.get_statistic_points(modified_model, modified_nncf_graph)
 
-            nncf_logger.info(">>> Collecting INT statistics for MinMax")
+            nncf_logger.info(">>> Collecting statistics for MinMax")
             min_max_statistics = self.collect_statistics(
                 min_max_statistic_points, modified_model, modified_nncf_graph, dataset
             )
@@ -168,31 +192,36 @@ class SmoothQuantTuner(Algorithm):
             modified_nncf_graph = NNCFGraphFactory.create(modified_model)
 
             int_container = self.get_container_for_nodes(
-                nodes_to_tune, self._algorithm_key, output_port_id, self._subset_size
+                nodes_list, self._algorithm_key, output_port_id, self._subset_size
             )
             nncf_logger.info(">>> Collecting INT statistics for tune")
             int_post_layer_statistics = self.collect_statistics(
                 int_container, modified_model, modified_nncf_graph, dataset
             )
 
-            for node_name in nodes_to_tune:
-                fp_statistics = self.get_statistics_for_node(
-                    fp_post_layer_statistics, node_name, self._algorithm_key, output_port_id
-                )
-                fp_statistics = fns.stack(fp_statistics)
-                int_statistics = self.get_statistics_for_node(
-                    int_post_layer_statistics, node_name, self._algorithm_key, output_port_id
-                )
-                int_statistics = fns.stack(int_statistics)
+            for group_id, nodes in node_groups.items():
+                current_loss = 0
+                for node_to_smooth in nodes:
+                    node_name = node_to_smooth.node_name
+                    fp_statistics = self.get_statistics_for_node(
+                        fp_post_layer_statistics, node_name, self._algorithm_key, output_port_id
+                    )[0]
+                    int_statistics = self.get_statistics_for_node(
+                        int_post_layer_statistics, node_name, self._algorithm_key, output_port_id
+                    )[0]
 
-                diff = fns.mean(fns.abs(fp_statistics - int_statistics)).data
+                    for fp_stat, int_stat in zip(fp_statistics, int_statistics):
+                        current_loss += fns.sum(fns.abs(fp_stat - int_stat) ** 2)
 
-                if diff < scale_by_node_name[node_name]["diff"]:
-                    scale_by_node_name[node_name] = {"diff": diff, "scale": scale}
+                if current_loss < group_losses[group_id]:
+                    group_losses[group_id] = current_loss
+                    for node in nodes:
+                        nodes_alpha[node.node_name] = alpha
+
             del int_post_layer_statistics
 
         nncf_logger.info(">>> Final quantization")
-        self._smooth_quant._alpha_map_by_layers = {n: d["scale"] for n, d in scale_by_node_name.items()}
+        self._smooth_quant._alpha_map_by_layers = nodes_alpha
         nncf_logger.info(self._smooth_quant._alpha_map_by_layers)
         print(self._smooth_quant._alpha_map_by_layers)
         model = self._smooth_quant.apply(model, graph, sq_statistics)
@@ -248,7 +277,7 @@ class SmoothQuantTuner(Algorithm):
             filter_func,
             key,
         ):
-            statistic = tensor_collector.get_statistics().mean_values
+            statistic = tensor_collector.get_statistics().values
             statistics_for_node.append(statistic)
         return statistics_for_node
 
@@ -257,7 +286,7 @@ class SmoothQuantTuner(Algorithm):
         container = StatisticPointsContainer()
         for node_name in node_names:
             target_point = OVTargetPoint(TargetType.POST_LAYER_OPERATION, node_name, output_port_id)
-            stat_collector = get_mean_statistic_collector(subset_size, -1)
+            stat_collector = get_raw_stat_collector(subset_size)
             container.add_statistic_point(
                 StatisticPoint(
                     target_point=target_point,
