009f4ccee2514d4b1c6a896e80abfbb78f0f1b63
Nikita Malinin <nikita.malinin@intel.com>
2025-06-04 11:41:10 +0200

    Remove old kernel

--- nncf/torch/quantization/reference.py
+++ nncf/torch/quantization/reference.py
@@ -126,12 +126,6 @@ torch_forward = CompilationWrapper(torch_executor.forward)
 torch_backward = CompilationWrapper(torch_executor.backward)
 
 
-class ReferenceGetter:
-    @classmethod
-    def get(cls, value):
-        return getattr(cls, value)
-
-
-class ReferenceQuantizedFunctions(ReferenceGetter):
+class ReferenceQuantizedFunctions:
     Quantize_forward = torch_forward
     Quantize_backward = torch_backward
