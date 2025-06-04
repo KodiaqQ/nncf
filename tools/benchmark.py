cd9842dd772ca31e92e6478af97d10dfc04d5094
Nikita Malinin <nikita.malinin@intel.com>
2025-06-04 12:46:52 +0200

    Fix

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -28,7 +28,6 @@ def warmup(layer, input_, runs, forward_only=False):
 
 def run_wall(layer, input_size_, device, runs, is_print=True, dtype=torch.float) -> dict[str, float]:
     input_ = torch.randn(input_size_, device=torch.device(device), dtype=dtype)
-    input_.requires_grad = True
 
     # Force CUDA initialization & warm up
     warmup(layer, input_, 100)
@@ -52,7 +51,6 @@ def run_wall(layer, input_size_, device, runs, is_print=True, dtype=torch.float)
 
 def run_profile(layer, input_size_, device, runs, forward_only=False, dtype=torch.float) -> dict[str, float]:
     input_ = torch.randn(input_size_, device=torch.device(device), dtype=dtype)
-    input_.requires_grad = True
 
     # Force CUDA initialization & warm up
     warmup(layer, input_, 100, forward_only)
